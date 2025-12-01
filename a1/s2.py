13ce8c4e64d73e41127941855b9e271124165161
AshtonCS <ashton.coventry-searle@student.uts.edu.au>
2025-12-01 17:21:33 +1100

    Renamed Files, Incorrect Approach to s2

---
+++ a1/s2.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+here = Path(__file__).parent
+input_path = here / "input.txt"
+
+moves = []
+
+try:
+    with open(input_path, 'r') as file:
+        for line in file:
+            moves.append([-1 if line.strip()[0] == 'L' else 1, int(line.strip()[1:])])
+except FileNotFoundError:
+    print(f"Error: The file '{input_path}' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+dial = 50
+password = 0;
+
+for m in moves:
+    movement = (dial + m[0] * m[1])
+    dial = movement % 100
+    overflow = int(movement / 100)
+    password += overflow if overflow > 0 else -1 * overflow
+
+print(password)
