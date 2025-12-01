6c9d0500f8bc614ae49bbad1ef562c10188f2a82
AshtonCS <ashton.coventry-searle@student.uts.edu.au>
2025-12-01 17:06:17 +1100

    Setup of key variables, file input

--- a1/main.py
+++ a1/main.py
@@ -3,12 +3,19 @@ from pathlib import Path
 here = Path(__file__).parent
 input_path = here / "input.txt"
 
+moves = []
+
 try:
     with open(input_path, 'r') as file:
         for line in file:
-            print(line)
+            moves.append([-1 if line.strip()[0] == 'L' else 1, int(line.strip()[1:])])
 except FileNotFoundError:
     print(f"Error: The file '{input_path}' was not found.")
 except Exception as e:
     print(f"An error occurred: {e}")
 
+dial = 50
+password = 0;
+
+for m in moves:
+    print(m)
