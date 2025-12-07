a39267f438d9546bfbb229da1d51ec6b1590b88f
AshtonCS <ashton.coventry-searle@student.uts.edu.au>
2025-12-07 18:02:39 +1100

    Correct answer for s2

--- a2/s2.py
+++ a2/s2.py
@@ -14,6 +14,7 @@ except Exception as e:
     print(f"An error occurred: {e}")
 
 invalid_sum = 0
+solutions = []
 
 for r in ranges:
     lb, ub = r.split('-')
@@ -21,24 +22,35 @@ for r in ranges:
     u_digits = len(ub)
     lower_bound = int(lb)
     upper_bound = int(ub)
-    for i in range((l_digits + 1) // 2, u_digits // 2 + 1, 2):
-        
-        first_i = 0
-        if(l_digits % 2 == 1):
-            first_i = int(math.pow(10, i-1))
-        else:
-            first_i = lower_bound // int(math.pow(10, i))
-        
-        last_i = 0
-        if(u_digits % 2 == 1):
-            last_i = int(math.pow(10, i)) - 1
-        else:
-            last_i = upper_bound // int(math.pow(10, i))
-        
-        for j in range(first_i, last_i + 1):
-            candidate = j * (int(math.pow(10, i)) + 1)
-            if candidate >= lower_bound and candidate <= upper_bound:
-                invalid_sum += candidate
+    for i in range(l_digits, u_digits + 1):
+        for j in range(1, i):
+            if i % j != 0:
+                continue
+            first_i = 0
+            if(l_digits % i != 0):
+                print('c')
+                first_i = int(math.pow(10, j-1))
+            else:
+                print('d')
+                first_i = lower_bound // int(math.pow(10, (i-j)))
+            
+            last_i = 0
+            if(u_digits % i != 0):
+                print('a')
+                last_i = int(math.pow(10, j)) - 1
+            else:
+                print('b')
+                last_i = upper_bound // int(math.pow(10, i-j))
+            
+            print(first_i, last_i, i, j)
+            for k in range(first_i, last_i + 1):
+                candidate = 0
+                for l in range(0, int(i/j)):
+                    candidate += k * int(math.pow(10, l * j))
+                if candidate >= lower_bound and candidate <= upper_bound and candidate not in solutions:
+                    print(" -- ", candidate, k)
+                    invalid_sum += candidate
+                    solutions.append(candidate)
 
 print(invalid_sum)
         
