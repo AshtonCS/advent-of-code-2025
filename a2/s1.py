faa41afb923daefb3778b24d6ea2f4cc1c1b0252
AshtonCS <ashton.coventry-searle@student.uts.edu.au>
2025-12-07 15:30:36 +1100

    First Attempt at a1, s1

--- a2/s1.py
+++ a2/s1.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import math
 
 here = Path(__file__).parent
 input_path = here / "input.txt"
@@ -12,4 +13,31 @@ except FileNotFoundError:
 except Exception as e:
     print(f"An error occurred: {e}")
 
-print(ranges)
+invalid_sum = 0
+
+for r in ranges:
+    lb, ub = r.split('-')
+    l_digits = len(lb)
+    u_digits = len(ub)
+    lower_bound = int(lb)
+    upper_bound = int(ub)
+    for i in range((l_digits + 1) // 2, u_digits // 2 + 1, 2):
+        
+        first_i = 0
+        if(l_digits % 2 == 1):
+            first_i = int(math.pow(10, i))
+        else:
+            first_i = lower_bound // int(math.pow(10, i))
+        
+        last_i = 0
+        if(u_digits % 2 == 1):
+            last_i = int(math.pow(10, i+1)) - 1
+        else:
+            last_i = upper_bound // int(math.pow(10, i))
+        
+        for j in range(first_i, last_i + 1):
+            invalid_sum += j * (int(math.pow(10, i)) + 1)
+
+print(invalid_sum)
+        
+        
