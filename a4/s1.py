46236869d8da03b79a17c27ca14d7b1db7c10795
AshtonCS <ashton.coventry-searle@student.uts.edu.au>
2025-12-18 17:48:27 +1100

    a4:s1 complete, using brute force strategy

--- a4/s1.py
+++ a4/s1.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import math
 
 here = Path(__file__).parent
-input_path = here / "prac.txt"
+input_path = here / "input.txt"
 
 grid = []
 
@@ -15,4 +15,19 @@ except FileNotFoundError:
 except Exception as e:
     print(f"An error occurred: {e}")
 
-print(grid)
+def neighbourCount(r, c):
+    counter = 0
+    for y in range(r-1, r+2):
+        if y >= 0 and y < len(grid):
+            for x in range(c-1, c+2):
+                if x >= 0 and x < len(grid[0])-1 and not (x == c and y == r) and grid[y][x] == '@':
+                    counter += 1
+    return counter
+
+if __name__ == "__main__":
+    total = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])-1):
+            if grid[row][col] == '@' and neighbourCount(row, col) < 4:
+                total += 1
+    print(total)
