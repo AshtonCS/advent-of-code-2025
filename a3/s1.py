8c3efa7e7592d99bbd61408ecc0a60e9239d6f17
AshtonCS <ashton.coventry-searle@student.uts.edu.au>
2025-12-18 16:32:29 +1100

    Added default files for Day 3 problems

---
+++ a3/s1.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import math
+
+here = Path(__file__).parent
+input_path = here / "input.txt"
+
+joltages = []
+
+try:
+    with open(input_path, 'r') as file:
+        for line in file:
+            joltages.append(str(line).strip("\n"))
+except FileNotFoundError:
+    print(f"Error: The file '{input_path}' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+print(joltages)
