e4a6f327b20d6555e34dd8bc17c79b38a9e31aab
AshtonCS <ashton.coventry-searle@student.uts.edu.au>
2025-12-19 15:02:29 +1100

    Revised s2 template, as all input is no longer required

--- a5/s2.py
+++ a5/s2.py
@@ -5,7 +5,6 @@ here = Path(__file__).parent
 input_path = here / "prac.txt"
 
 quality = []
-available = []
 
 try:
     with open(input_path, 'r') as file:
@@ -13,12 +12,10 @@ try:
             vals = line.split('-')
             if len(vals) == 2:
                 quality.append([int(vals[0]), int(vals[1])])
-            elif line != "\n":
-                available.append(int(line))
 
 except FileNotFoundError:
     print(f"Error: The file '{input_path}' was not found.")
 except Exception as e:
     print(f"An error occurred: {e}")
 
-print(quality, available)
+print(quality)
