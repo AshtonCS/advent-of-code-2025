d33c2d5d77366ff05f518022c9b659aa6ff8a9b5
AshtonCS <ashton.coventry-searle@student.uts.edu.au>
2025-12-20 15:37:22 +1100

    Almost correct solution for s2, need to reevaluate results on operations with different digits

--- a6/s2.py
+++ a6/s2.py
@@ -18,4 +18,35 @@ except Exception as e:
 
 total = 0
 
+for p in range(len(problems[0])):
+    sign = problems[len(problems)-1][p]
+    split_nums = [[' ' for _ in range(4)] for _ in range(len(problems) - 1)]
+
+    for i in range(len(problems) - 1):
+        digits = list(problems[i][p])
+        if(sign == '*'):
+            digits.reverse()
+        split_nums[i][:len(digits)] = digits
+
+    ceph_nums = ["" for _ in range(4)]
+    for num in split_nums:
+        for digit in range(len(num)):
+            if digit != ' ':
+                ceph_nums[digit] += num[digit]
+
+    subtotal = 0
+    if(sign == '*'):
+        subtotal = 1
+    
+    for num in ceph_nums:
+        if len(num.split()) != 0:
+            if sign == '+':
+                subtotal += int(num)
+            else:
+                subtotal *= int(num)
+        
+    print("Numbers: ", ceph_nums, ", Subtotal: ", subtotal, ", Operator: ", sign)
+    total += subtotal
+
 print(total)
+
