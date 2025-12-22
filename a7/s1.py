535a51ea79dfcfd43f2a34de36c81f7cb9d52b73
AshtonCS <ashton.coventry-searle@student.uts.edu.au>
2025-12-22 11:27:34 +1100

    Correct a7-s1, using classic brute-force strategy

--- a7/s1.py
+++ a7/s1.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import math
 
 here = Path(__file__).parent
-input_path = here / "input.txt"
+input_path = here / "prac.txt"
 
 diagram = []
 
@@ -16,4 +16,19 @@ except FileNotFoundError:
 except Exception as e:
     print(f"An error occurred: {e}")
 
-print(diagram)
+active_cols = [False] * len(diagram[0])
+active_cols[diagram[0].find('S')] = True
+counter = 0
+
+for row in range(1, len(diagram)):
+    for col in range(len(diagram[0])):
+        if diagram[row][col] == '^' and active_cols[col]:
+            if col != 0:
+                active_cols[col-1] = True
+            active_cols[col] = False
+            if col != len(diagram[0]) - 1:
+                active_cols[col+1] = True
+            counter += 1
+
+print(counter)
+
